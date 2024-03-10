b253c1f386078589594a1574f97de468a2466b47
Mike Ooi <mic3060@gmail.com>
2024-03-10 23:34:06 +1100

    feat: add microbatcher logic feat: add shutdown method feat: removed job return and implemented func interface instead so batchprocessor can be a dependant chore: refactor model

--- microbatcher.go
+++ microbatcher.go
@@ -2,30 +2,83 @@ package microbatcher
 
 import (
 	"time"
-
-	"github.com/fernglin/microbatcher/models"
 )
 
+// Incoming job to be sent for processing
+type Job struct {
+	Data interface{}
+}
+
 type MicroBatcher struct {
-	batchSize  int             // Maximum number of jobs per batch
-	batchFreq  time.Duration   // Maximum time.Duration before a batch is sent for processing
-	jobsCh     chan models.Job // Channel for incoming jobs
-	shutdownCh chan struct{}   // Channel to recieve shutdown signal
+	batchSize        int           // Maximum number of jobs per batch
+	batchFreq        time.Duration // Maximum time.Duration before a batch is sent for processing
+	jobsCh           chan Job      // Channel for incoming jobs
+	funcProcessBatch func([]Job)   // Function to process batches when ready
+	shutdownCh       chan struct{} // Channel to recieve shutdown signal
 }
 
 // Instantiate a new MicroBatcher with a specific batch size and frequency.
-func NewMicroBatcher(batchSize int, batchFreq time.Duration) *MicroBatcher {
+func NewMicroBatcher(batchSize int, batchFreq time.Duration, funcProcessBatch func([]Job)) *MicroBatcher {
 	return &MicroBatcher{
-		batchSize:  batchSize,
-		batchFreq:  batchFreq,
-		jobsCh:     make(chan models.Job),
-		shutdownCh: make(chan struct{}),
+		batchSize:        batchSize,
+		batchFreq:        batchFreq,
+		jobsCh:           make(chan Job),
+		shutdownCh:       make(chan struct{}),
+		funcProcessBatch: funcProcessBatch,
 	}
 }
 
 // Submits a job to the MicroBatcher.
-func (mb *MicroBatcher) SubmitJob(job models.Job) models.JobReturn {
-	returnCh := make(chan models.JobReturn)
+func (mb *MicroBatcher) SubmitJob(job Job) {
 	mb.jobsCh <- job
-	return <-returnCh
+}
+
+// Starts the MicroBatcher
+func (mb *MicroBatcher) Start() {
+	var batch []Job
+	var batchTimer *time.Timer
+
+	for {
+		select {
+		// Jobs coming into jobs channel
+		case job := <-mb.jobsCh:
+			batch = append(batch, job)
+			// Send job for processing once batch size limit is reached
+			if len(batch) >= mb.batchSize {
+				mb.processBatch(batch)
+				batch = nil
+				// Reset batchTimer once batch is processed
+				if batchTimer != nil {
+					batchTimer.Stop()
+					batchTimer = nil
+				}
+				// If batchTimer is nil, start a new timer with duration defined in batchFrequency
+			} else if batchTimer == nil {
+				batchTimer = time.NewTimer(mb.batchFreq)
+				go func() {
+					// Process the batch if timer has completed and no further jobs are received
+					<-batchTimer.C
+					mb.processBatch(batch)
+					batch = nil
+				}()
+			}
+		// Shutdown signal received. Send job for processing.
+		case <-mb.shutdownCh:
+			if len(batch) > 0 {
+				mb.processBatch(batch)
+			}
+			return
+		}
+	}
+
+}
+
+// Send job for processing by BatchProcessor as a callback
+func (mb *MicroBatcher) processBatch(batch []Job) {
+	mb.funcProcessBatch(batch)
+}
+
+// Method to shut down the MicroBatcher
+func (mb *MicroBatcher) Shutdown() {
+	close(mb.shutdownCh)
 }
